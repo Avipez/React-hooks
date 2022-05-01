7e4efa1eb86e60b23ee9d26899429e6f13b36597
David Lopez <dg_lf@hotmail.com>
2022-04-30 21:32:08 -0500

    avances hasta clase 10

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,4 +1,5 @@
-import {useState, useEffect, useReducer} from 'react';
+import {useState, useEffect, useReducer, useMemo, useRef, useCallback} from 'react';
+import Search from './Search';
 
 const initialState = {
     favorites: []
@@ -20,6 +21,16 @@ const Characters = () => {
 
     const [characters, setCharacters] = useState([]);
     const [favorites, dispatch] = useReducer(favoriteReducer, initialState);
+    const [search, setSearch] = useState("");
+    const searchInput = useRef(null);
+
+    /* const handleSearch = () => {
+        setSearch(searchInput.current.value)
+    }; */
+
+    const handleSearch = useCallback (() => {
+        setSearch(searchInput.current.value)
+    }, [])
 
     useEffect(() => {
         fetch("https://rickandmortyapi.com/api/character/")
@@ -29,10 +40,22 @@ const Characters = () => {
 
     const handleClick = (favorite) => {
         dispatch({ type: "ADD_TO_FAVORITE", payload: favorite})
-    }
+    };
+
+    /* const filterCharacters = characters.filter((char) => {
+        return char.name.toLowerCase().includes(search.toLowerCase());
+    }); */
+
+    const filterCharacters = useMemo( ()=> 
+        characters.filter((char) => {
+            return char.name.toLowerCase().includes(search.toLowerCase());
+        }), [characters, search]
+    )
 
     return (
         <div className="characters">
+
+            <Search search={search} searchInput={searchInput} handleSearch={handleSearch} />
 
             {favorites.favorites.map( favorite => (
                 <li key={favorite.id}>
@@ -40,7 +63,7 @@ const Characters = () => {
                 </li>
             ))}
 
-            {characters.map(character => (
+            {filterCharacters.map(character => (
                 <div className="item" key={character.id}>
                     <h2>{character.name}</h2>
                     <button type='button' onClick={() => (handleClick(character))}> Agregar a favoritos </button>
